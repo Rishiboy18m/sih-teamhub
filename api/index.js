934d0eb050a31019f5221697e1637fca22de55a8
Rishiboy18m <rishiboy18m@gmail.com>
2026-08-23 15:24:38 +0530

    Add serverless error diagnostic wrapper in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-const app = require('../server/index');
-
-module.exports = (req, res) => {
-  return app(req, res);
+module.exports = async (req, res) => {
+  try {
+    const app = require('../server/index');
+    return app(req, res);
+  } catch (err) {
+    console.error('Vercel serverless error:', err);
+    return res.status(500).json({
+      error: 'Vercel Serverless Invocation Error',
+      message: err.message,
+      stack: err.stack
+    });
+  }
 };
